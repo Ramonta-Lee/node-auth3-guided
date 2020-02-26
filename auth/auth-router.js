16a962500b0d29dbd91703a0d4ec337b10a7d5cf
unknown <viola4lfe@gmail.com>
2020-02-26 15:43:29 -0500

    Completed solution

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); // npm library
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
+const { jwtSecret } = require("../config/secrets.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +20,43 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(({ name, code, message, stack }) => {
+      res.status(500).json({ name, code, message, stack });
     });
 });
+
+// may want to bring this function in from somewhere else
+// you can put data in the payload
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // "sub" property
+    username: user.username,
+    role: user.role || "user"
+    // ...other data
+  };
+  // const secret = process.env.JWT_SECRET || "keep it secret, keep it safe"
+  const options = {
+    expiresIn: "8h"
+    // checkout the library for more options
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/*require("dotenv").config({
+  path: "full/custom/path/to/your/env/vars"
+}); */
+require("dotenv").config();
+
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+// nodemon looks for this file and then runs the server information
